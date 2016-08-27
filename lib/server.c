76f33043fab638ee0dbc7cbf605efcb1d29d6f6a
Andy Green <andy@warmcat.com>
2016-08-27 17:07:06 +0800

    per-vhost headers and lwsws conf support
    This l;ets you add per-vhost arbitrary headers on served files, eg
    
         "headers": [{
            "X-Content-Type-Options": "nosniff",
            "X-XSS-Protection": "1; mode=block",
            "x-frame-options": "SAMEORIGIN"
            }],

--- lib/server.c
+++ lib/server.c
@@ -407,10 +407,12 @@ lws_http_serve(struct lws *wsi, char *uri, const char *origin,
 			/* we don't need to send the payload */
 			if (lws_add_http_header_status(wsi, 304, &p, end))
 				return -1;
+
 			if (lws_add_http_header_by_token(wsi,
 					WSI_TOKEN_HTTP_ETAG,
 					(unsigned char *)sym, n, &p, end))
 				return -1;
+
 			if (lws_finalize_http_header(wsi, &p, end))
 				return -1;
 
